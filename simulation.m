00ae578f65e3a5d14c724640bb9ff8320b06e08f
Wei-Han Chen (Alex) <alex81527@gmail.com>
2024-02-12 20:44:24 -0500

    update

--- simulation.m
+++ simulation.m
@@ -46,5 +46,19 @@ steer = steervec(pa.getElementPosition()/(physconst('LightSpeed')/60.48e9), ...
 load("../mmw-calibration-sim/cal32_new_taoffice.mat"); % somhow this still works for node 1 mod 5
 % PA.PHASE_CAL(PA.ACTIVE_ANT) = calibration_vec;
 new_steer = steer./exp(1j.*calibration_vec.');
-figure;
-imagesc(abs(new_steer'*steer)); colorbar;
+rng(4);
+new_steer2 = steer./exp(1j.*2*pi*rand(32,1));
+% new_steer4 = exp(1j.*2*pi*rand(32,1));
+
+figure(1);
+xlabel("ground truth angle");
+subplot(131); tmp = abs(steer'*steer).'; imagesc(tmp./max(tmp)); colorbar;
+subplot(132); tmp = abs(new_steer'*steer).'; imagesc(tmp./max(tmp)); colorbar;
+subplot(133); tmp = abs(new_steer2'*steer).'; imagesc(tmp./max(tmp)); colorbar;
+
+figure;plot(abs(new_steer2(:,162)'*steer))
+
+load("../mmw-calibration-sim/newcal_700.mat");
+a1 = exp(1j.*calibration_vec.');
+a2 = exp(1j.*cal_refant_26);
+figure; plot(angle([a1 a2]))
